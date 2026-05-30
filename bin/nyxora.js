ca58c33fa87e595ca045ec34656d3d3cba3a12d7
nyxoraAI <yudhantoprasetyo@gmail.com>
2026-05-30 18:55:21 +0700

    fix: explicit TS_NODE_PROJECT for global installs to resolve tsconfig moduleResolution errors

--- bin/nyxora.js
+++ bin/nyxora.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
+const path = require('path');
 process.env.TS_NODE_SKIP_IGNORE = "true";
 process.env.TS_NODE_TRANSPILE_ONLY = "true";
+process.env.TS_NODE_PROJECT = path.join(__dirname, '../tsconfig.json');
 require('ts-node/register');
 require('../launcher.ts');
