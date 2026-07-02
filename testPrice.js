9f165323d55e9f1ef101191e0a337d69d8d8c11b
Nyxora Protocol <64382466+nyxoraAI@users.noreply.github.com>
2026-07-02 12:48:23 +0700

    Delete testPrice.js

--- testPrice.js
+++
@@ -1,4 +0,0 @@
-import { getPrice } from './packages/core/dist/web3/skills/getPrice.js';
-getPrice('0x22f7Cb13D44e6Da722D92D137A7635d05b206A96', undefined, 3821)
-  .then(console.log)
-  .catch(console.error);
