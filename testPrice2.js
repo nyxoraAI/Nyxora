f68bd0300eee4bac3bd4b7047779bc374c87b00c
Nyxora Protocol <64382466+nyxoraAI@users.noreply.github.com>
2026-07-02 12:48:35 +0700

    Delete testPrice2.js

--- testPrice2.js
+++
@@ -1,6 +0,0 @@
-const { loadConfig } = require('./packages/core/dist/config/parser.js');
-const { getPrice } = require('./packages/core/dist/web3/skills/getPrice.js');
-
-getPrice('0x22f7Cb13D44e6Da722D92D137A7635d05b206A96', 'idr', 3821)
-  .then(console.log)
-  .catch(console.error);
